b02b1b688f98a52ec76b014d297b248f1f98cc85
Hadiyo11 <hadyhope@gmail.com>
2023-10-13 18:31:02 -0700

    new work

--- MERN/movie-reviews/backend/api/movies.route.js
+++ MERN/movie-reviews/backend/api/movies.route.js
@@ -2,8 +2,6 @@ import express from "express";
 import MoviesController from './movies.controller.js';
 import ReviewsController from './reviews.controller.js';
 
-
-
 const router = express.Router(); // get access to express router
 
 router.route('/').get(MoviesController.apiGetMovies);
--- MERN/movie-reviews/backend/server.js
+++ MERN/movie-reviews/backend/server.js
@@ -6,8 +6,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1/movies", movies);
 
+
+app.use("/api/v1/movies", movies);
 app.use("*", (req, res) => {
   res.status(404).json({ error: "not found" });
 });
