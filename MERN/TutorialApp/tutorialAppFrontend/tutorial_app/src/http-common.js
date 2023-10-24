d3a01dd12c22017f7a96f916f888069abd300576
Hadiyo11 <hadyhope@gmail.com>
2023-10-24 09:37:19 -0700

    new work

--- MERN/TutorialApp/tutorialAppFrontend/tutorial_app/src/http-common.js
+++ MERN/TutorialApp/tutorialAppFrontend/tutorial_app/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://tutorial-app-3vx8.onrender.com/api",
   headers: {
     "Content-type": "application/json"
   }
--- MERN/TutorialApp/tutorialAppFrontend/tutorial_app/src/services/tutorial-service.js
+++ MERN/TutorialApp/tutorialAppFrontend/tutorial_app/src/services/tutorial-service.js
@@ -2,31 +2,31 @@ import http from "../http-common";
 
 class TutorialDataService {
   getAll() {
-    return http.get("/tutorials");
+    return http.get("https://tutorial-app-3vx8.onrender.com/tutorials");
   }
 
   get(id) {
-    return http.get(`/tutorials/${id}`);
+    return http.get(`https://tutorial-app-3vx8.onrender.com/tutorials/${id}`);
   }
 
   create(data) {
-    return http.post("/tutorials", data);
+    return http.post("https://tutorial-app-3vx8.onrender.com/tutorials", data);
   }
 
   update(id, data) {
-    return http.put(`/tutorials/${id}`, data);
+    return http.put(`https://tutorial-app-3vx8.onrender.com/tutorials/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`/tutorials/${id}`);
+    return http.delete(`https://tutorial-app-3vx8.onrender.com/tutorials/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`/tutorials`);
+    return http.delete(`https://tutorial-app-3vx8.onrender.com/tutorials`);
 }
 
 findByTitle(title) {
-  return http.get(`/tutorials?title=${title}`);
+  return http.get(`https://tutorial-app-3vx8.onrender.com/tutorials?title=${title}`);
 }
 }
 
