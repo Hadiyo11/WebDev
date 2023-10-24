b655d29c46dc50ddb6c1ff4960d11e68e17eb695
Hadiyo11 <hadyhope@gmail.com>
2023-10-24 08:35:02 -0700

    new work

--- MERN/TutorialApp/tutorialAppFrontend/tutorial_app/src/actions/types.js
+++
@@ -1,6 +0,0 @@
-//defined string constants that indicates the type of actions perfomed
-export const CREATE_TUTORIAL = "CREATE_TUTORIAL";
-export const RETRIEVE_TUTORIALS = "RETRIEVE_TUTORIALS";
-export const UPDATE_TUTORIAL = "UPDATE_TUTORIAL";
-export const DELETE_TUTORIAL = "DELETE_TUTORIAL";
-export const DELETE_ALL_TUTORIALS = "DELETE_ALL_TUTORIALS";
