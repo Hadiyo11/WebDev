96111aecc95b1aba1802548396194b176f00ae6d
Hadiyo11 <hadyhope@gmail.com>
2023-11-01 11:09:53 -0700

    new work

--- MERN/TutorialApp/tutorialAppFrontend/tutorial_app/src/services/tutorial-service.js
+++ MERN/TutorialApp/tutorialAppFrontend/tutorial_app/src/services/tutorial-service.js
@@ -2,7 +2,7 @@ import http from "../http-common";
 
 class TutorialDataService {
   getAll() {
-    return http.get("https://tutorial-app-c6lr.onrender.com/api/tutorials");
+    return http.get("https://tutorial-app-pijh.onrender.com/api/tutorials");
   }
 
   // get(id) {
@@ -10,7 +10,7 @@ class TutorialDataService {
   // }
 
   get(id) {
-    return http.get(`https://tutorial-app-c6lr.onrender.com/api/tutorials/${id}`)
+    return http.get(`https://tutorial-app-pijh.onrender.com/api/tutorials/${id}`)
      .catch(error => {
       console.error("Error fetching tutorial:", error);
       throw error;
@@ -18,23 +18,23 @@ class TutorialDataService {
   }
 
   create(data) {
-    return http.post("https://tutorial-app-c6lr.onrender.com/api/tutorials", data);
+    return http.post("https://tutorial-app-pijh.onrender.com/api/tutorials", data);
   }
 
   update(id, data) {
-    return http.put(`https://tutorial-app-c6lr.onrender.com/api/tutorials/${id}`, data);
+    return http.put(`https://tutorial-app-pijh.onrender.com/api/tutorials/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`https://tutorial-app-c6lr.onrender.com/api/tutorials/${id}`);
+    return http.delete(`https://tutorial-app-pijh.onrender.com/api/tutorials/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`https://tutorial-app-c6lr.onrender.com/api/tutorials`);
+    return http.delete(`https://tutorial-app-pijh.onrender.com/api/tutorials`);
   }
 
   findByTitle(title) {
-    return http.get(`https://tutorial-app-c6lr.onrender.com/api/tutorials?title=${title}`);
+    return http.get(`https://tutorial-app-pijh.onrender.com/api/tutorials?title=${title}`);
   }
 }
 
