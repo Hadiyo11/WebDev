96ec277cde7e1a0e29b7a8141c6560f8a13c4c5b
Hadiyo11 <hadyhope@gmail.com>
2023-11-01 10:31:23 -0700

    new work

--- MERN/TutorialApp/tutorialAppFrontend/tutorial_app/src/http-common.js
+++ MERN/TutorialApp/tutorialAppFrontend/tutorial_app/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://tutorial-app-uaok.onrender.com/api/tutorials",
+  baseURL: "https://tutorial-app-heui.onrender.com/api/tutorials",
   headers: {
     "Content-type": "application/json"
   }
